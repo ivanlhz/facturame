72690cbfc5b93f33592401e34b1b42f9d481a3b3
Iván López Hdez <ivanlhz@gmail.com>
2019-01-20 12:08:10 +0000

    Add pageTitle component

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,21 @@
+<template>
+ <v-container>
+    <v-layout wrap>
+      <v-flex md12>
+          <div>
+            <div class="display-2 blue--text font-weight-light">Facturame!:<span class="display-1 primary--text font-weight-thin"> genera tus facturas en PDF</span></div>  
+          </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+
+
+<script>
+  export default {
+    data: () =>({
+      
+    })
+  }
+</script>
